77269fddc4a2b6f35a2627137a215cc3b7132811
EugeneZaharchenko <eugenezaharchenko@gmail.com>
2018-11-09 19:46:51 +0200

    modules are being installed

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cnf = require('../package.json').config;
+var autoprefixer = require('gulp-autoprefixer');
+var cssnano = require('gulp-cssnano')
 // sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
     return gulp.src(cnf.src.sass)
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 4 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
         .pipe(gulp.dest(cnf.dist.css));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/*.scss', ['sass']);
+    gulp.watch('./src/sass/**/*.*', ['sass']);
 });
