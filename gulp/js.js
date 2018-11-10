55b4bfb318dc73942b2c3c33fb1671c9a1889369
EugeneZaharchenko <eugenezaharchenko@gmail.com>
2018-11-10 22:22:41 +0200

    JS: babel, include & so on

--- gulp/js.js
+++ gulp/js.js
@@ -2,17 +2,22 @@ var gulp        = require('gulp');
 var sass        = require('gulp-sass');
 var cnf         = require('../package.json').config;
 var plumber     = require('gulp-plumber');
-var notify = require("gulp-notify");
-var sourcemaps = require('gulp-sourcemaps');
-
+var notify      = require("gulp-notify");
+var sourcemaps  = require('gulp-sourcemaps');
+var babel       = require('gulp-babel');
+var include     = require("gulp-include");
 
 gulp.task('js', function () {
     return gulp.src(cnf.src.js)
         .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
-        .pipe(sourcemaps.init())
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(babel())
+        .pipe(include({
+            extensions: "js",
+            hardFail: true}))
 
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(cnf.dist.css));
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(cnf.dist.js));
 });
 
 gulp.task('js:watch', function () {
